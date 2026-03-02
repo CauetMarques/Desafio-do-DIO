9888fb0ecfe93fe807682440c67bbf3e15d5679c
CauetMarques <cauete0712@gmail.com>
2026-03-02 19:01:35 -0300

    first commit

---
+++ DesafioJavascript.js
@@ -0,0 +1,32 @@
+let NomeDoHeroi="Heroi Generico"
+let exp=8000
+if(exp<=1000){
+    console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Ferro")
+}
+else if(exp<=2000){
+    console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Bronze")
+}
+else if(exp<=5000){
+ console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Prata")
+}
+else if(exp<=7000){
+     console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Ouro")
+}
+else if(exp<=8000){
+     console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Platina")
+} 
+
+else if(exp<=9000){
+     console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Ascendente")
+}
+
+else if(exp<=10000){
+     console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Imortal")
+}
+else if(exp>=10000){
+     console.log("O Heroi de nome: "+NomeDoHeroi+", esta no nivel Radiante")
+}
+
+else{
+    console.log("undefined")
+}
